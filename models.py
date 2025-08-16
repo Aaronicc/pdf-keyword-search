60c065338158e645f50ee05c7be369c61418e75f
Aaronicc <santiagodarwinaaroon@gmail.com>
2025-08-16 19:52:15 +0800

    Delete models.py

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Keyword(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    word = db.Column(db.String(100), unique=True, nullable=False)
-    positive = db.Column(db.Boolean, default=True)
